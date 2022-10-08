814c3da329214d5f5fba1e59749badc6f53018f3
guyllaumedemers <guyllaume_demers@hotmail.fr>
2022-10-08 12:01:09 -0400

    running config.cmd on current setup. fixing issues as we go! try_run{} current failed but other issues were fixed

---
+++ src/appTetris/Main.cpp
@@ -0,0 +1,12 @@
+#include "libImGUI.cpp"
+#include "libSDL2.cpp"
+
+void libImGUITest();
+void libSDL2Test();
+
+int main(int argv, char** argc)
+{
+    libImGUITest();
+    libSDL2Test();
+    return 0;
+}
