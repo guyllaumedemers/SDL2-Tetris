fd9c04387aa3cca7d335aca7ba254aff7cde487f
guyllaumedemers <guyllaume_demers@hotmail.fr>
2022-10-15 14:22:18 -0400

    pushing work on cmake setup - building sdl2 lib using buildsystem - removing dependencies duplicated - disabled CMakeLists retrieved from sdl2 submodules in build - outputing .dlls required by the executable in the proper dir POST_BUILD

--- src/appTetris/Main.cpp
+++ src/appTetris/Main.cpp
@@ -1,9 +1,44 @@
-void libImGUITest();
-void libSDL2Test();
+#include <SDL.h>
+#include <cstdlib>
 
-int main(int argv, char** argc)
+void libImGUITest();
+//void libSDL2Test();
+
+int main(int argc, char** argv)
 {
     libImGUITest();
-    libSDL2Test();
+    //libSDL2Test();
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0 /*0 is Success here*/)
+    {
+        SDL_LogError(SDL_LOG_PRIORITY_CRITICAL, "ERROR: SDL2 INIT FAILED!");
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* Window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, NULL);
+
+    if(!Window)
+    {
+        SDL_LogError(SDL_LOG_PRIORITY_CRITICAL, "ERROR: SDL2 WINDOW CREATION FAILED!");
+        return EXIT_FAILURE;
+    }
+
+    bool bIsWindowOpen = true;
+
+    while(bIsWindowOpen)
+    {
+        SDL_Event Event;
+	    if(SDL_WaitEvent(&Event) > 0)
+	    {
+		    if(Event.type == SDL_QUIT)
+		    {
+                bIsWindowOpen = false;
+		    }
+	    }
+    }
+
+    SDL_DestroyWindow(Window);
+    SDL_Quit();
+
     return 0;
 }
